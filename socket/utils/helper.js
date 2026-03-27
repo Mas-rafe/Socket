ea6a9d9937b966085d625c4a049518342e3bba11
Masrafe Efte <eftemasrafe@gmail.com>
2026-03-27 16:40:35 +0600

    updated at helper.js

--- socket/utils/helper.js
+++ socket/utils/helper.js
@@ -1,9 +1,19 @@
 export function validateOrder(data) {
 
-    if(!data.customerName?.trim()){
-return {
-    valid: false, message:'Customer name is required';
-}
+    if (!data.customerName?.trim()) {
+        return {
+            valid: false, message: 'Customer name is required'
+        }
     }
-    
+    if (!data.customerPhone?.trim()) {
+        return {
+            valid: false, message: 'Customer phone number is required'
+        }
+    }
+    if (!data.customerAddress?.trim()) {
+        return {
+            valid: false, message: 'Customer address is required'
+        }
+    }
+
 }
